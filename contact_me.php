67ea780f32f492b42e5dba9c7edb50383cfa02f6
parvister <parham.parvizi@github.com>
2017-10-11 16:06:43 -0700

    changing contact_me.php to use updated Pear Mail module. Fixed gmail send error.

--- contact_me.php
+++ contact_me.php
@@ -1,15 +1,16 @@
 <?php
 
-$my_file = '/tmp/contact_me.log';
-$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+require_once "Mail.php";
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$log = "name: $name, email: $email, message: $message\n";
-fwrite($handle, $data);
-fclose($handle);
+// $my_file = '/tmp/contact_me.log';
+// $handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+// $log = "name: $name, email: $email, message: $message\n";
+// fwrite($handle, $log);
+// fclose($handle);
 
 // Check for empty fields
 if(empty($_POST['name'])                ||
@@ -21,14 +22,36 @@ if(empty($_POST['name'])                ||
    }
 
 
+$from = $email;
+$to = '<info@tura.io>';
+$subject = "tura.io website contact from: $name";
+$body = "From: $name\nEmail: $email\nMessage:\n$message";
 
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'info@tura.io',
+        'password' => '1nf0teamtura'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+// old send mail
 // Create the email and send the message
-$to = "info@tura.io";
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nContact: $email\n\nMessage:\n$message";
-$headers = "From: $email\n";
-$headers .= "Reply-To: $email";
-mail($to,$email_subject,$email_body,$headers);
+// $to = "info@tura.io";
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nContact: $email\n\nMessage:\n$message";
+// $headers = "From: $email\n";
+// $headers .= "Reply-To: $email";
+// mail($to,$email_subject,$email_body,$headers);
+
 echo "sent mail.";
 return true;
 ?>
